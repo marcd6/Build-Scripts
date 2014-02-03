347012cb1c13c8665448e38252f998026d7ee819
Drew Walker <drew.walker@gmail.com>
2014-02-03 19:28:47 +1100

    Moved .vdi to Build-Assets/

--- 2.create-virtual-machine.sh
+++ 2.create-virtual-machine.sh
@@ -1,5 +1,5 @@
 VBoxManage createvm --name "$1" --register
-VBoxManage modifyvm "$1" --memory 512 --acpi on --nic1 bridged --bridgeadapter1 "en1: Wi-Fi (AirPort)"
-VBoxManage createhd --filename $1.vdi --size 10000
+VBoxManage modifyvm "$1" --memory 512 --acpi on --nic1 bridged --bridgeadapter1 "en0: Wi-Fi (AirPort)"
+VBoxManage createhd --filename Build-Assets/$1.vdi --size 10000
 VBoxManage storagectl "$1" --name "IDE Controller" --add ide
-VBoxManage storageattach "$1" --storagectl "IDE Controller" --port 0 --device 0 --type hdd --medium $1.vdi
+VBoxManage storageattach "$1" --storagectl "IDE Controller" --port 0 --device 0 --type hdd --medium Build-Assets/$1.vdi
