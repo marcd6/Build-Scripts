fe77f5b337e2fce39c96d1cc93d391da06085223
Drew Walker <drew.walker@gmail.com>
2014-02-03 19:27:16 +1100

    Don't download VirtualBox if it's already in Build-Assets/

--- 1.install-virtual-machine-host.sh
+++ 1.install-virtual-machine-host.sh
@@ -1,6 +1,16 @@
-#wget http://download.virtualbox.org/virtualbox/4.3.6/VirtualBox-4.3.6-91406-OSX.dmg
-curl -OL http://download.virtualbox.org/virtualbox/4.3.6/VirtualBox-4.3.6-91406-OSX.dmg
+#!/bin/bash
+if [ -f Build-Assets/VirtualBox-4.3.6-91406-OSX.dmg ];
+then
+    echo "File exists, no need to download"
+else
+    echo "File does not exist, downloading..."
+    if [ ! -d "Build-Assets" ];
+    then
+        echo "Creating Build-Assets directory"
+        mkdir "Build-Assets"
+    fi
+    curl -Lo Build-Assets/VirtualBox-4.3.6-91406-OSX.dmg http://download.virtualbox.org/virtualbox/4.3.6/VirtualBox-4.3.6-91406-OSX.dmg
+fi
 hdiutil mount VirtualBox-4.3.6-91406-OSX.dmg
-sudo installer -package /Volumes/VirtualBox/VirtualBox.pkg -target /Volumes/HD
+sudo installer -package /Volumes/VirtualBox/VirtualBox.pkg -target "/Volumes/Macintosh HD"
 hdiutil unmount /Volumes/VirtualBox
-rm VirtualBox-4.3.6-91406-OSX.dmg
