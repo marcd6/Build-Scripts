14af585b21157a59b068161b8871ca7a2ea35798
Drew Walker <drew.walker@gmail.com>
2014-02-04 20:56:50 +1100

    Added ability to switch to static IP from DHCP or other BOOTPROTO

--- assign-static-ip-address.sh
+++ assign-static-ip-address.sh
@@ -1,5 +1,7 @@
 echo "NETWORKING=yes" > /etc/sysconfig/network
 echo "HOSTNAME=$1" >> /etc/sysconfig/network
 echo "GATEWAY=$3" >> /etc/sysconfig/network
+sed -i s/BOOTPROTO=.*/BOOTPROTO=static/g /etc/sysconfig/network-scripts/ifcfg-eth0
 echo "IPADDR=$2" >> /etc/sysconfig/network-scripts/ifcfg-eth0
 echo "NETMASK=$4" >> /etc/sysconfig/network-scripts/ifcfg-eth0
+/etc/init.d/network restart
